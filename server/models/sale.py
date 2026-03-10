6aa7336e8d58790fe1e0358bb2dea6dc56427edc
Ashlin Rofina <ashlinrofina@gmail.com>
2026-03-10 17:58:53 +0300

    creates sales model

--- server/models/sale.py
+++ server/models/sale.py
@@ -1,2 +1,17 @@
+from server.app import db
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import relationship
+
 class Sale():
-    pass
+    __tablename__="sales"  
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,  db.ForeignKey("users.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Decimal, nullable=False)  
+
+    order = db.relationship("Order", bacl_populates="sales")
+    
+
+     
